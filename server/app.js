ab7617ce961de19cc7fcf3b8c4cbcd69c7f22bad
Murat Akkus <akkusmurat123@gmail.com>
2024-11-19 15:02:51 +0100

    i runned as build command another command

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
